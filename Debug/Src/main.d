360ea48e6e8f68c0da8888e5cf3c2274d55ab221
tflaemig68 <83691371+tflaemig68@users.noreply.github.com>
2024-03-16 12:10:47 +0100

    Initial commit

---
+++ Debug/Src/main.d
@@ -0,0 +1,5 @@
+Src/main.o: ../Src/main.c \
+ C:/GITHUB/STM32/EDS/Balancer/Inc/RotaryPushButton.h \
+ C:/GITHUB/STM32/EDS/Balancer/Inc/Balancer.h
+C:/GITHUB/STM32/EDS/Balancer/Inc/RotaryPushButton.h:
+C:/GITHUB/STM32/EDS/Balancer/Inc/Balancer.h:
